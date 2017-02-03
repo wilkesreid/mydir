10347b36dbcf6bcdfecbbca09f047c3453149f74
Samuel Reid <Sam@Sams-Mac.local>
2017-02-03 13:00:26 -0600

    First iteration where it actually works

--- README.md
+++ README.md
@@ -1,19 +1,70 @@
 # mydir
 
-Easily register and recall aliases for commonly used directories.
+** Built for Mac OS X **
+
+mydir is a command line tool that can help keep track of commonly used folders without cluttering up your environment variables. I often find that it's tedious to call `cd` over and over to get to the folder of the project I'm working in. This way I can easily and intuitively set an alias for my project folder.
 
 # Table of Contents
 
+- [Prerequisites](#prerequisites)
 - [Installation](#installation)
+- [Tutorial](#tutorial)
 - [Usage](#usage)
 
+## Prerequisites
+
+You must have [npm/nodejs](https://docs.npmjs.com/getting-started/installing-node) installed.
 
 ## Installation
 
-`npm install mydir`
+`npm install mydir -g`
+
+In order to be able to call `cmydir <alias>` to instantly change to an aliased directory, run this in terminal after installation:
+
+`. ~/.bash_profile`
+
+## Tutorial
+
+Create a project folder for yourself:
+
+`mkdir -p ~/Documents/Projects/MyProject`
+
+Create an alias with `mydir`:
+
+`mydir set MyProject ~/Documents/Projects/MyProject`
+
+Change to your project directory from any other directory:
+
+`cmydir MyProject`
 
 ## Usage
 
+### Set
+
+To create an alias, use the `set` command:
+
+`mydir set <alias> <path>`
+
+For example:
+
 `mydir set MyProject ~/Documents/Projects/MyProject`
+
+** *Note*: the path provided must be absolute. **
+
+### Recall an alias
+
+To use an alias you've created, just call `mydir` with your alias as the second argument:
+
+`mydir <alias>`
+
+For example:
+
+`mydir MyProject`
+
+The main usage of this is to `cd` into an aliased folder. This is how you would do that:
+
+`cmydir MyProject`
+
+Here's what's happening behind the scenes:
 
 `cd $(mydir MyProject)`
--- mydir.js
+++ mydir.js
@@ -1,7 +1,77 @@
 #! /usr/bin/env node
 
 const fs = require('fs');
+const mkdirp = require('mkdirp');
+const source = require('shell-source');
 
-fs.access('/usr/local', fs.constants.R_OK && fs.constants.W_OK, (err) => {
-  console.log(err ? 'no access!' : 'can read/write');
+if (!/^darwin/.test(process.platform)) {
+  console.log("mydir is currently built only for Mac OS X");
+  return;
+}
+
+if (process.argv.length < 3) {
+  console.log("requires an argument");
+  return;
+}
+
+mkdirp('/usr/local/var/mydir', (mkdirp_err) => {
+  if (!mkdirp_err) {
+
+    fs.open('/usr/local/var/mydir/list.json', 'a+', (open_err, fd) => {
+      if (!open_err) {
+        // Do stuff
+
+        var list = fs.readFileSync('/usr/local/var/mydir/list.json', 'utf8');
+
+        if (list == "") {
+          list = "{}";
+          fs.writeFileSync('/usr/local/var/mydir/list.json', list, 'utf8');
+        }
+
+        var data = JSON.parse(list);
+
+        switch (process.argv[2]) {
+          case 'alias':
+            source(__dirname + '/mydir-alias.sh', (err)=>{});
+          break;
+          case 'list':
+            for (var key in data) {
+              if (!data.hasOwnProperty(key)) continue;
+
+              process.stdout.write(key+": "+data[key]+"\n");
+            }
+          break;
+          case 'set':
+            if (process.argv.length < 5) {
+              process.stdout.write("'set' requires two additional arguments.\n");
+            } else {
+              data[process.argv[3]] = process.argv[4];
+              fs.writeFileSync('/usr/local/var/mydir/list.json', JSON.stringify(data), 'utf8');
+            }
+          break;
+          case 'rm':
+            if (process.argv.length < 4) {
+              process.stdout.write("'rm' required one additional argument.\n");
+            } else {
+              delete data[process.argv[3]];
+              fs.writeFileSync('/usr/local/var/mydir/list.json', JSON.stringify(data), 'utf8');
+            }
+          break;
+          default:
+            if (data.hasOwnProperty(process.argv[2])) {
+              process.stdout.write(data[process.argv[2]]);
+            } else {
+              process.stdout.write(".");
+            }
+          break;
+        }
+
+      } else {
+        console.log("error: "+err);
+      }
+    });
+
+  } else {
+    console.log("could not create/access /usr/local/var/mydir");
+  }
 });
